2eb7f4125af116341af21bb36206da8e3a08b958
guptasanil <guptasanil2504@gmail.com>
2021-01-02 18:51:57 +0000

    added UserCard as component

--- src/App.js
+++ src/App.js
@@ -1,105 +1,47 @@
 import React, {UseState, useEffect, useState} from 'react';
 import { Form, Card, Image, Icon } from 'semantic-ui-react';
-import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery';
+import UserCard from '../src/Components/UserCard'
+
+
 
 
 
 
 function App() {
 
-  const [name, setName] = useState(' ');
-  const [userName, setUserName] = useState(' ');
-  const [followers, setFollowers] = useState(' ');
-  const [following, setFollowing] = useState(' ');
-  const [repos, setRepos] = useState(' ');
-  const [avatar, setAvatar] = useState(' ');
-  const [userInput, setUserInput] = useState(' ');
-  const [error, setError] = useState(null);
-
-
-
-  useEffect(() => { 
-    fetch('https://api.github.com/users/guptasanil') 
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
-      console.log(data);
-    });
-  }, []);
-
-  const setData = ({ name, login, followers, following, public_repos, avatar_url}) => {
-      setName(name)
-      setUserName(login)
-      setFollowers(followers)
-      setFollowing(following)
-      setRepos(public_repos)
-      setAvatar(avatar_url)
-
-
-  }
-
+  const [userInput, setUserInput] = useState('guptasanil');
+  const [userName, setUserName] = useState('guptasanil');
+  
   const handleSearch = (e) => {
-      setUserInput(e.target.value)
-  } 
+    setUserInput(e.target.value)
 
-  const handleSubmit = () => {
-    fetch(`https://api.github.com/users/${userInput}`)
-      .then (res => res.json())
-      .then (data => {
-        if(data.message) {
-          setError(data.message)
-        }
-        else{
-        setData(data);
-        setError(null)
-        }
-  })
 }
+
+const handleSubmit = () => {
+  setUserName(userInput)
+}
+  
 
   return(
     <div>
       <div className= 'navbar'>Github Search</div> 
       <div className= "search">
-        <Form onSubmit = {handleSubmit}>
-          <Form.Group>
-            <Form.Input placeholder='Github User' name='Github User' onChange = {handleSearch} /> 
-            <Form.Button content='Search' />
-          </Form.Group>
-        </Form>
-      </div>
-      {error ? (<h1>{error}</h1>) : (<div className= "card"> 
-        <Card>
-          <Image src={avatar} wrapped ui={false} />
-            <Card.Content>
-      <Card.Header>{name}</Card.Header>
-      <Card.Header>{userName}</Card.Header>
-     
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        {followers} followers
-      </a>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        {repos} repos
-      </a>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        {following} following
-      </a>
-    </Card.Content>
-  </Card>
-
-      </div>)  }
+            <Form onSubmit = {handleSubmit}>
+              <Form.Group>
+                <Form.Input placeholder='Search Github User' name='Github User' onChange = {handleSearch} /> 
+                <Form.Button content='Search' />
+              </Form.Group>
+            </Form>
+          </div>
+      <UserCard data = {userName} />    
       
-    </div>
+       
+ 
+
+    </div> 
+      
+    
   );
 }
 
---
+++ src/Components/UserCard.js
@@ -0,0 +1,82 @@
+import { Form, Card, Image, Icon } from 'semantic-ui-react';
+import React, { useEffect, useState} from "react";
+import "../App.css";
+
+function UserCard ({ data }){
+
+
+  const [name, setName] = useState(' ');
+  const [userName, setUserName] = useState(' ');
+  const [followers, setFollowers] = useState(' ');
+  const [following, setFollowing] = useState(' ');
+  const [repos, setRepos] = useState(' ');
+  const [avatar, setAvatar] = useState(' ');
+ 
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState('guptasanil');
+
+
+
+  useEffect(() => { 
+    getData();
+  }, [ data ]);
+
+  const getData = () => {
+    fetch(`https://api.github.com/users/${data}`)
+    .then (res => res.json())
+    .then (data => {
+      if(data.message) {
+        setError(data.message)
+      }
+      else{
+      setData(data);
+      setError(null)
+      }
+    }
+    )
+  }
+
+  const setData = ({ name, login, followers, following, public_repos, avatar_url}) => {
+      setName(name)
+      setUserName(login)
+      setFollowers(followers)
+      setFollowing(following)
+      setRepos(public_repos)
+      setAvatar(avatar_url)
+
+
+  }
+        return(
+            <Card>
+          <Image src={avatar} wrapped ui={false} />
+            <Card.Content>
+      <Card.Header>{name}</Card.Header>
+      <Card.Header>{userName}</Card.Header>
+     
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        {followers} followers
+      </a>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        {repos} repos
+      </a>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        {following} following
+      </a>
+    </Card.Content>
+  </Card>
+
+        )
+
+    }
+
+
+export default UserCard;
